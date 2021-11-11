c232d288a9e04a833a5d77133f35ca2a12f8770c
marcelino230 <marcelinookta@gmail.com>
2021-11-11 15:45:16 +0700

    youtube-clone

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1X5NnW2QnEoKqblXZJY_LBaxT4okcHws",
+  authDomain: "clone-edf62.firebaseapp.com",
+  projectId: "clone-edf62",
+  storageBucket: "clone-edf62.appspot.com",
+  messagingSenderId: "779261789260",
+  appId: "1:779261789260:web:8f387a27b36d8ddfcd20e9",
+  measurementId: "G-X1JNX0L06N"
+};
